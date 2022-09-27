fd96ad974ef242de1c0f1ac1857f5e11cd6d1209
Arun Prasad <aprasadr7@gmail.com>
2022-09-27 17:44:47 +0530

    API Controller

--- src/main/java/com/aws/deploy/APIContorller.java
+++ src/main/java/com/aws/deploy/APIContorller.java
@@ -1,12 +1,20 @@
 package com.aws.deploy;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class APIContorller {
 
+    private final String LOCALHOST_IPV4 = "127.0.0.1";
+    private final String LOCALHOST_IPV6 = "0:0:0:0:0:0:0:1";
     @GetMapping("api/health")
     public ResponseEntity<?> healthCheck()
     {
@@ -20,8 +28,41 @@ public class APIContorller {
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> home()
+    public ResponseEntity<?> home(HttpServletRequest request)
     {
-        return ResponseEntity.ok("App Home");
+
+        ModelAndView model = new ModelAndView("index");
+        String clientIp = getClientIp(request);
+        return ResponseEntity.ok("App Home :: " + clientIp);
+    }
+
+    public String getClientIp(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if(StringUtils.isEmpty(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+
+        if(StringUtils.isEmpty(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if(StringUtils.isEmpty(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+            if(LOCALHOST_IPV4.equals(ipAddress) || LOCALHOST_IPV6.equals(ipAddress)) {
+                try {
+                    InetAddress inetAddress = InetAddress.getLocalHost();
+                    ipAddress = inetAddress.getHostAddress();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if(!StringUtils.isEmpty(ipAddress)
+                && ipAddress.length() > 15
+                && ipAddress.indexOf(",") > 0) {
+            ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+        }
+        return  ipAddress;
     }
 }
