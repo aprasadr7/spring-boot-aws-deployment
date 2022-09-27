33087587f22919e8b4338f003bc5ce470012fc89
Arun Prasad <aprasadr7@gmail.com>
2022-09-27 10:05:25 +0530

    Spring Boot App init

---
+++ src/main/java/com/aws/deploy/APIContorller.java
@@ -0,0 +1,21 @@
+package com.aws.deploy;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIContorller {
+
+    @GetMapping("api/health")
+    public ResponseEntity<?> healthCheck()
+    {
+        return ResponseEntity.ok("It works successfully.");
+    }
+
+    @GetMapping("api/check")
+    public ResponseEntity<?> checkUp()
+    {
+        return ResponseEntity.ok("Up and Running.");
+    }
+}
